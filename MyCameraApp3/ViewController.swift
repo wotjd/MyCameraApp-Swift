4aae20e6335c737a7bb43ef4a693bdfa3679269e
wotjd <ju8625@castis.com>
2018-08-06 16:34:57 +0900

    Initial Commit

---
+++ MyCameraApp3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyCameraApp3
+//
+//  Created by wotjd on 2018. 8. 6..
+//  Copyright © 2018년 wotjd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
